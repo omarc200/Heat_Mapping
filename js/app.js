874aae46c5fa4e9ae804cb06c663110ec9b0413b
Omar <omarcarrillo238@gmail.com>
2026-03-06 22:56:34 -0500

    3D is coded and the toggle button works

--- js/app.js
+++ js/app.js
@@ -5,12 +5,22 @@ require([
   "esri/views/SceneView",
   "esri/widgets/Home",
   "esri/widgets/ScaleBar",
-  "esri/widgets/Fullscreen"
-], function(Map, SceneView, Home, ScaleBar, Fullscreen) {
+  "esri/widgets/Fullscreen",
+  "esri/layers/SceneLayer"
+], function(Map, SceneView, Home, ScaleBar, Fullscreen, SceneLayer) {
 
   const map = new Map({
     basemap: "streets-vector"
   });
+// Creates 3D layer esri recomends portal item instead of URL we can test later
+  const open3DBuildings = new SceneLayer({
+    portalItem: {
+        id: "c444b24b184c4523a5dc96248bfea4e1"
+    },
+    visible: false
+  });
+
+  map.add(open3DBuildings);
 
   const view = new SceneView({
     container: "viewDiv",
@@ -45,4 +55,21 @@ const basemapSelect = document.getElementById("basemapSelect");
   basemapSelect.addEventListener("change", function() {
     map.basemap = basemapSelect.value;
   });
+  
+let is3DMode = false;
+
+const toggleButton = document.getElementById("toggle3D");
+
+toggleButton.addEventListener("click", function(){
+    is3DMode = !is3DMode;
+
+    if(is3DMode){
+        open3DBuildings.visible = true;
+        toggleButton.textContent = "Switch to 2D";
+    } else {
+        open3DBuildings.visible = false;
+        toggleButton.textContent = "Switch to 3D";
+    }
+});
+
 });
